8ac0457b250b47397a5206cdaec1b02a10e58743
Jaricapa-holberton <2241@holbertonschool.com>
2021-06-25 14:57:07 -0500

    Class State created

--- models/city.py
+++ models/city.py
@@ -6,3 +6,16 @@
       - state_id: string - empty string: it will be the State.id
       - name: string - empty string
 """
+from models.base_model import BaseModel
+import uuid
+from datetime import datetime
+class State(BaseModel):
+      """A state class for storing user data, which inherits from BaseModel."""
+      def __init__(self, *args, **kwargs):
+        """The class constructor.
+        Args:
+            *args: Variable length argument list.
+            **kwargs: Arbitrary keyword arguments.
+        """
+      super().__init__(id)
+      self.name = str("")
--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -1,3 +1,53 @@
 #!/usr/bin/python3
 # -*- coding:utf-8 -*-
 """Unit test for state.py"""
+from models.base_model import BaseModel
+from unittest import TestCase
+from datetime import datetime
+import uuid
+import inspect
+import pep8
+
+class TestState(TestCase):
+    """Test cases for State class."""
+
+    def setUp(self):
+        """Setup for State tests."""
+        self.base_1 = State()
+
+    def test_instance(self):
+        """Test for correct instancing of State object."""
+        self.assertIsInstance(self.base_1, State)
+    
+class TestBaseModelDoc(TestCase):
+    "Tests documentation and pep8 for State class."
+
+    @classmethod
+    def setUpClass(cls):
+        """Sets the whole functions of the class State to be inspected for
+        correct documentation."""
+        cls.functions = inspect.getmembers(State,
+                                           inspect.isfunction(State))
+
+    def test_doc_module(self):
+        """Tests for docstring presence in the module and the class."""
+        from models import base_model
+
+        self.assertTrue(len(base_model.__doc__) > 0)
+        self.assertTrue(len(base_model.State.__doc__) > 0)
+
+    def test_doc_fun(self):
+        """Tests for docstring presence in all functions of class."""
+        for fun in self.functions:
+            self.assertTrue(len(fun.__doc__) > 0)
+
+    def test_pep8(self):
+        """Tests pep8 style compliance of module and test files."""
+        p8 = pep8.StyleGuide(quiet=False)
+
+        res = p8.check_files(['models/base_model.py'])
+        self.assertEqual(res.total_errors, 0,
+                         "Found code style errors (and warnings).")
+        res = p8.check_files(['tests/test_models/test_base_model.py'])
+        self.assertEqual(res.total_errors, 0,
+                         "Found code style errors (and warnings).")
