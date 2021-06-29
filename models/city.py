ac715fd8109d5f8e8afce371caffeb7aa2d79388
Alfredo Delgado Moreno <alfredodlgdmrn@gmail.com>
2021-06-29 21:58:57 +0000

    Fix: attribute name on City class

--- models/city.py
+++ models/city.py
@@ -3,7 +3,7 @@
 """city module.
 - Class City - A city class to save data of the users
 - Class Attributes (public):
-      - city_id: The city's id.
+      - state_id: The state's id.
       - name: The city's name.
 """
 from models.base_model import BaseModel
@@ -12,8 +12,8 @@ from models.base_model import BaseModel
 class City(BaseModel):
     """A city class for storing user data, which inherits from BaseModel.
     Attributes:
-        city_id (str): The city id.
+        state_id (str): The state's id.
         name (str): The city's name.
     """
-    city_id = ""
+    state_id = ""
     name = ""
